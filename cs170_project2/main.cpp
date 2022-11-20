2ca4134a601ef2531df2f152da5a9a609155e070
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2022-11-19 16:32:51 -0800

    Initial Commit

---
+++ cs170_project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs170_project2
+//
+//  Created by Marshall Jones on 11/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
